1cc6431ddd8937043be2b2f414cbaadf2fd2fd98
githubadhaiskhazifa <62179765+githubadhaiskhazifa@users.noreply.github.com>
2020-04-13 15:40:40 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,18 @@
+<div class="col-md-12">
+    <ol class="breadcrumb">
+        <li><a href="index.php" class="text-menu text-primary "><i class="fa fa-home text-menu"></i> Home</a></li>
+    </ol>
+</div>
+
+<div class="col-md-12 mb-4">
+    <div class="card">
+        <div class="bg-primary text-light"><h5><i class="fa fa-home m-2"></i> Beranda</h5></div>
+        <div class="card-body">
+
+            <p><center><img src="./image/spk1.png" width="900" height="300">
+        </div>
+        <div class="card-footer">
+            <i class="text-secondary" style="font-size: 10px">Copyright &copy; 2019 : Anwarsyah/IF-4-15(2019)</i>
+        </div>
+    </div>
+</div>
